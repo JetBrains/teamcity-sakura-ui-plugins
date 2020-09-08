73482dee2848f0f17996a702161166d24f0bf29d
Denis Lesnik <denis.lesnik@jetbrains.com>
2020-09-08 17:20:54 +0200

    TeamcityReactApi -> TeamCityAPI

--- demoPlugin-server/src/main/resources/buildServerResources/controlled-plugin-jsp.js
+++ demoPlugin-server/src/main/resources/buildServerResources/controlled-plugin-jsp.js
@@ -1,7 +1,7 @@
 (() => {
     console.log("My Controlled plugin script from a JSP file");
 
-    const plugin = TeamcityReactAPI.pluginRegistry.searchByPlaceId("SAKURA_PROJECT_BEFORE_CONTENT", "SakuraUI-Plugin")
+    const plugin = TeamCityAPI.pluginRegistry.searchByPlaceId("SAKURA_PROJECT_BEFORE_CONTENT", "SakuraUI-Plugin")
 
     const template = (context) => `<div class="controlled-plugin-wrapper">Here is a controlled plugin.${JSON.stringify(context)}</div>`
 
