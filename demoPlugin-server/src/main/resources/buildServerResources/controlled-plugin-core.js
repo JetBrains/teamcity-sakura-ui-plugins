c18abe29127fcb61d424d4c0e8767b01af8fb06e
Denis Lesnik <denis.lesnik@jetbrains.com>
2020-09-08 22:00:31 +0200

    Added a way to make TeamCityAPI available within different EAP versions

--- demoPlugin-server/src/main/resources/buildServerResources/controlled-plugin-core.js
+++ demoPlugin-server/src/main/resources/buildServerResources/controlled-plugin-core.js
@@ -1,6 +1,10 @@
 (() => {
     console.log("My Controlled plugin script from a Core file");
 
+    if (!TeamCityAPI) {
+        TeamCityAPI = window.TeamcityReactAPI
+    }
+
     const plugin = TeamCityAPI.pluginRegistry.searchByPlaceId("SAKURA_PROJECT_BEFORE_CONTENT", "SakuraUI-Plugin")
 
     const template = (context) => `<div class="controlled-plugin-wrapper">Here is a controlled plugin.${JSON.stringify(context)}</div>`
