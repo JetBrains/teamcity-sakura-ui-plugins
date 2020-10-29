ac02ae3eba6115b2cebf98664a69a122f0777c67
Denis Lesnik <denis.lesnik@jetbrains.com>
2020-10-29 11:51:04 +0100

    Divided the Plugin Development flow for the Classic and for the Sakura UI accordingly the Core Plugin updates.

--- demoPlugin-server/src/main/java/com/demoDomain/teamcity/demoPlugin/controllers/SakuraUIPluginController.java
+++ demoPlugin-server/src/main/java/com/demoDomain/teamcity/demoPlugin/controllers/SakuraUIPluginController.java
@@ -11,6 +11,14 @@ public class SakuraUIPluginController {
             @NotNull PluginDescriptor descriptor,
             @NotNull PagePlaces places
             ) {
-        new SimplePageExtension(places, PlaceId.SAKURA_HEADER_NAVIGATION_AFTER, PLUGIN_NAME, descriptor.getPluginResourcesPath("basic-plugin.jsp")).addCssFile("basic-plugin.css").register();
+
+        // For the Sakura UI we register the Plugin using the SAKURA-prefixed PlaceIDs. Keep in mind the new syntax:
+        // new PlaceID(String) - constructor accepts any string. Plugins for SAKURA-prefixed PlaceIDs are available only
+        // in the Sakura UI and are loading asynchronously.
+        new SimplePageExtension(places, new PlaceId("SAKURA_BEFORE_CONTENT"), PLUGIN_NAME, descriptor.getPluginResourcesPath("basic-plugin.jsp")).addCssFile("basic-plugin.css").register();
+
+        // For the Classic UI we continue using the regular PlaceIds. Those plugins are rendered on the Server
+        // and, generally speaking, stay the same, as they were the last decade
+        new SimplePageExtension(places, PlaceId.BEFORE_CONTENT, PLUGIN_NAME, descriptor.getPluginResourcesPath("basic-plugin.jsp")).addCssFile("basic-plugin.css").register();
     }
 }
--- demoPlugin-server/src/main/resources/buildServerResources/basic-plugin.jsp
+++ demoPlugin-server/src/main/resources/buildServerResources/basic-plugin.jsp
@@ -1,9 +1,2 @@
-<%@ include file="/include.jsp" %>
-
-<c:if test="${not empty buildType}">
-    <div>
-        The selected build configuration: <c:out value="${buildType.name}"/>
-    </div>
-</c:if>
-
 <div class="basic-plugin-wrapper">Here is a basic plugin.</div>
+<script>console.log("Here is the basic plugin script")</script>
