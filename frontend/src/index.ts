733a744df20539a5bb4106fd3e2311faf3c03606
Denis Lesnik <denis.lesnik@jetbrains.com>
2020-10-31 13:44:27 +0100

    Added unified way to register a plugin both in the old and the new UI

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -2,13 +2,8 @@ import Plugin from "@jetbrains/teamcity-api/plugin"
 import App from './App/App'
 import AllBuilds from "./AllBuilds/AllBuilds";
 
-new Plugin(Plugin.placeIds.SAKURA_SIDEBAR_TOP, {
+// @ts-ignore
+new Plugin([Plugin.placeIds.SAKURA_SIDEBAR_TOP, Plugin.placeIds.BEFORE_CONTENT], {
     name: "Sakura UI Plugin",
     content: App,
-})
-
-new Plugin(Plugin.placeIds.SAKURA_PROJECT_BEFORE_CONTENT, {
-    name: "Sakura UI Plugin",
-    content: AllBuilds,
-})
-
+});
