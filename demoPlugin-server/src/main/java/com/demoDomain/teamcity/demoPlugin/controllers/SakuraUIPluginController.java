f75519dd9431b15ffc3b78f24effdeaee865d2c5
Denis Lesnik <denis.lesnik@jetbrains.com>
2020-12-01 14:11:50 +0100

    2020.2 Release update: - Use internal property to pass the bundle.js URL - Changed TeamCity OpenAPI Version to 2020.2 - Removed redundant and not clear code for CSP usage

--- demoPlugin-server/src/main/java/com/demoDomain/teamcity/demoPlugin/controllers/SakuraUIPluginController.java
+++ demoPlugin-server/src/main/java/com/demoDomain/teamcity/demoPlugin/controllers/SakuraUIPluginController.java
@@ -1,8 +1,6 @@
 package com.demoDomain.teamcity.demoPlugin.controllers;
 
 import jetbrains.buildServer.controllers.BaseController;
-import jetbrains.buildServer.serverSide.ProjectManager;
-import jetbrains.buildServer.serverSide.SBuildType;
 import jetbrains.buildServer.web.ContentSecurityPolicyConfig;
 import jetbrains.buildServer.web.openapi.*;
 import jetbrains.buildServer.web.openapi.PluginDescriptor;
@@ -16,34 +14,31 @@ import javax.servlet.http.HttpServletResponse;
 
 public class SakuraUIPluginController extends BaseController {
     private static final String PLUGIN_NAME = "SakuraUI-Plugin";
-    private static final String BUNDLE_DEV_URL = "http://localhost:8080";
 
     private final PluginDescriptor myPluginDescriptor;
 
     public SakuraUIPluginController(
             @NotNull PluginDescriptor descriptor,
             @NotNull PagePlaces places,
-            @NotNull WebControllerManager controllerManager,
-            @NotNull final ContentSecurityPolicyConfig contentSecurityPolicyConfig
+            @NotNull WebControllerManager controllerManager
     ) {
         myPluginDescriptor = descriptor;
 
         String url = "/reactPlugin.html";
         final SimplePageExtension pageExtension = new SimplePageExtension(places);
         pageExtension.setPluginName(PLUGIN_NAME);
-        pageExtension.setPlaceId(PlaceId.ALL_PAGES_FOOTER);
+        pageExtension.setPlaceId(PlaceId.ALL_PAGES_FOOTER_PLUGIN_CONTAINER);
         pageExtension.setIncludeUrl(url);
         pageExtension.register();
 
         controllerManager.registerController(url, this);
-        contentSecurityPolicyConfig.addDirectiveItems("script-src", BUNDLE_DEV_URL);
     }
 
     @Nullable
     @Override
     protected ModelAndView doHandle(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response) throws Exception {
         final ModelAndView mv = new ModelAndView(myPluginDescriptor.getPluginResourcesPath("react-plugin.jsp"));
-        mv.getModel().put("BUNDLE_DEV_URL", BUNDLE_DEV_URL);
+        mv.getModel().put("PLUGIN_NAME", PLUGIN_NAME);
         return mv;
     }
 }
--- demoPlugin-server/src/main/resources/buildServerResources/react-plugin.jsp
+++ demoPlugin-server/src/main/resources/buildServerResources/react-plugin.jsp
@@ -1,10 +1,13 @@
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"
+<%@ page import="jetbrains.buildServer.util.StringUtil"
+%><%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"
 %><%@ taglib prefix="intprop" uri="/WEB-INF/functions/intprop"
 %><%@ include file="/include.jsp" %>
 
+<c:set var="overrideBundleUrl" value="${intprop:getProperty('teamcity.plugins.SakuraUI-Plugin.bundleUrl', '')}" />
+
 <c:choose>
-  <c:when test="${BUNDLE_DEV_URL ne null}">
-    <script src="<c:out value="${BUNDLE_DEV_URL}" />/bundle.js" />
+  <c:when test="${!StringUtil.isEmpty(overrideBundleUrl)}">
+    <script src="<c:out value="${overrideBundleUrl}" />"></script>
   </c:when>
   <c:otherwise>
     <bs:linkScript>${teamcityPluginResourcesPath}bundle.js</bs:linkScript>
