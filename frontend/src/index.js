e12c53f5fa5a8a7a448d63c08692432db4dfb66a
Denis Lesnik <denis.lesnik@jetbrains.com>
2020-12-02 15:19:02 +0100

    2020.2 Release update: Updated plugin constructor to use Array of PlaceIDs Updated TeamCity Api JS module version

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,8 @@
 // @flow strict
 import {Plugin, React} from "@jetbrains/teamcity-api"
 import App from './App/App'
-import AllBuilds from "./AllBuilds/AllBuilds";
 
-new Plugin(Plugin.placeIds.SAKURA_SIDEBAR_TOP, {
+new Plugin([Plugin.placeIds.SAKURA_SIDEBAR_TOP, Plugin.placeIds.BEFORE_CONTENT], {
     name: "Sakura UI Plugin",
     content: App,
-});
-
-new Plugin(Plugin.placeIds.SAKURA_PROJECT_BEFORE_CONTENT, {
-    name: "Sakura UI Plugin",
-    content: AllBuilds,
 });
